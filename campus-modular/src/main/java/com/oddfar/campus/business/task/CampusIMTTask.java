c6886447376779422d48ce8fae3cb204f9bee092
60 <1096364408@qq.com>
2024-02-26 22:57:54 +0800

    Update CampusIMTTask.java

--- campus-modular/src/main/java/com/oddfar/campus/business/task/CampusIMTTask.java
+++ campus-modular/src/main/java/com/oddfar/campus/business/task/CampusIMTTask.java
@@ -48,7 +48,7 @@ public class CampusIMTTask {
      * 9点期间，每分钟执行一次
      */
     @Async
-    @Scheduled(cron = "0 * 9,22 ? * *")
+    @Scheduled(cron = "0 * * ? * *")
     public void reservationBatchTask() {
         logger.info("「预约」开始预约  ");
         imtService.reservationBatch();
